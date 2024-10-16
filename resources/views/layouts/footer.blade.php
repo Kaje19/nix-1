075f5752fc4f27332718265be88022227d6137a2
Kaje19 <106543155+Kaje19@users.noreply.github.com>
2024-10-16 20:27:24 +0800

    Update the footer addresss

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,9 @@
 <footer>
     <div class="address">
-        <span class="title">the studio</span>
-        <span class="content">Door 1, FDS Bldg. Corner Lukewright, Perdices Street, Dumaguete City</span>
+        <span class="title">the studio</span><br>
+        <span class="content">Door 1, FDS Bldg. <span class="nowrap">Corner&nbsp;Lukewright</span>, Perdices Street, Dumaguete City</span>
     </div>
+
 
     <div class="title">
         &copy;2020 Nix Productions & Events
@@ -13,10 +14,10 @@
         <span class="email">anaconnicam.gonzales.ict@gmail.com</span>
         <ul class="socials">
             <li class="item">
-                <img src="assets/images/Facebook.png" alt="">
+                <img src="assets/images/Facebook.png" alt="Facebook Icon">
             </li>
             <li class="item">
-                <img src="assets/images/Instagram.png" alt="">
+                <img src="assets/images/Instagram.png" alt="Instagram Icon">
             </li>
         </ul>
     </div>
